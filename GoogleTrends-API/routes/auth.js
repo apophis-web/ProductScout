333e602931f07a29e73a60d04f5e18299e8e5a69
apophis-web <hassanwaqar4830@gmail.com>
2023-04-19 12:27:05 +0500

    major commit

--- GoogleTrends-API/routes/auth.js
+++ GoogleTrends-API/routes/auth.js
@@ -2,29 +2,29 @@ const express = require('express');
 const router = express.Router();
 const googleTrends = require('google-trends-api');
 
-// router.post('/trends', async (req,res) =>{
-//   googleTrends.interestOverTime({keyword: req.body.trendfor})
-//   .then(function(results){
-//     return res.status(200).send({'trends': JSON.parse(results)});
-//   })
-//   .catch(function(err){
-//     res.status(404).send(err);
-//   });
-
-// });
-
-router.post('/trends', async (req, res) => {
-  googleTrends.interestOverTime({
-    keyword: req.body.trendfor,
-    geo: 'PK'
+router.post('/trends', async (req,res) =>{
+  googleTrends.interestOverTime({keyword: req.body.trendfor})
+  .then(function(results){
+    return res.status(200).send({'trends': JSON.parse(results)});
   })
-    .then(function(results) {
-      return res.status(200).send({ trends: JSON.parse(results) });
-    })
-    .catch(function(err) {
-      res.status(404).send(err);
-    });
+  .catch(function(err){
+    res.status(404).send(err);
+  });
+
 });
+
+// router.post('/trends', async (req, res) => {
+//   googleTrends.interestOverTime({
+//     keyword: req.body.trendfor,
+//     geo: 'PK'
+//   })
+//     .then(function(results) {
+//       return res.status(200).send({ trends: JSON.parse(results) });
+//     })
+//     .catch(function(err) {
+//       res.status(404).send(err);
+//     });
+// });
 
 
 module.exports = router;
